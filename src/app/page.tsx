0118dd4040cb4a58806bfe92c094f5a884207810
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2025-04-20 15:40:36 +0900

    refactor: 랜딩 페이지에서 HomePage 제거 및 구조 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import HomePage from '@/features/home';
+import { Session1 } from '@/features/home/components/Session1';
+import { Session2 } from '@/features/home/components/Session2';
+import { Session3 } from '@/features/home/components/Session3';
+import { Session4 } from '@/features/home/components/Session4';
 
 export default function home() {
   return (
     <main className="flex flex-col items-center">
-      <HomePage />
+      <Session1 />
+      <Session2 />
+      <Session3 />
+      <Session4 />
     </main>
   );
 }
