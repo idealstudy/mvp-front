fe200f91387cafa94e4a1b0f63faf253afe35968
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2025-04-21 19:31:37 +0900

    fix: 누락된 page.tsx 파일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Session1 } from '@/features/home/components/Session1';
-import { Session2 } from '@/features/home/components/Session2';
-import { Session3 } from '@/features/home/components/Session3';
-import { Session4 } from '@/features/home/components/Session4';
+import { Session1 } from '@/features/home/components/session-1';
+import { Session2 } from '@/features/home/components/session-2';
+import { Session3 } from '@/features/home/components/session-3';
+import { Session4 } from '@/features/home/components/session-4';
 
 export default function home() {
   return (
