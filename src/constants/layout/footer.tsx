41a5824393e63a9e97d43a3133fd8d36142ab0bd
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2025-04-24 14:31:34 +0900

    feat: header/footer 추가, 이미지 네이밍 정리 및 CSS 수정

---
+++ src/constants/layout/footer.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import logo from '@/../public/logo.svg';
+import { ROUTE } from '@/constants/route';
+
+export const Footer = () => {
+  const router = useRouter();
+
+  return (
+    <footer className="w-full bg-[#2E2E2E] px-5 py-[68px] text-sm text-white">
+      <div className="mx-auto max-w-7xl space-y-4">
+        <Image
+          src={logo}
+          alt="THE EDU 로고"
+          width={79}
+          height={22}
+          className="cursor-pointer"
+          role="button"
+          onClick={() => router.push(ROUTE.HOME)}
+        />
+
+        <p className="mt-[31px]">
+          <span className="font-semibold">팀 디에듀</span> | 조성진 강이규
+          김나래 김다혜 김대민 김효인 나경주 백인빈 신상호 오황석 장우성
+        </p>
+
+        <p>
+          <span className="font-semibold">이메일</span> |{' '}
+          <a href="mailto:support@dedu.kr">support@dedu.kr</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
---
+++ src/constants/layout/header.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import profile from '@/../public/header-profile.svg';
+import logo from '@/../public/logo.svg';
+import { ROUTE } from '@/constants/route';
+
+export const Header = () => {
+  const router = useRouter();
+
+  // 로그인 여부 확인 수정 예정
+  const isLoggedIn = false;
+
+  const buttonBase =
+    'cursor-pointer border border-[#1A1A1A] px-6 py-3 text-base font-bold text-white';
+
+  return (
+    <header className="border-b border-gray-200 bg-[#1A1A1A] px-4 py-3">
+      <div className="mx-auto flex max-w-[1385px] items-center justify-between">
+        <Image
+          src={logo}
+          alt="THE EDU 로고"
+          width={79}
+          height={22}
+          className="cursor-pointer"
+          role="button"
+          onClick={() => router.push(ROUTE.HOME)}
+        />
+        {isLoggedIn ? (
+          <Image
+            src={profile}
+            alt="프로필 사진"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        ) : (
+          <div className="flex gap-5">
+            <Link
+              role="button"
+              href={ROUTE.LOGIN}
+              className={buttonBase}
+            >
+              로그인
+            </Link>
+            <Link
+              role="button"
+              href={ROUTE.SIGNUP.HOME}
+              className={`${buttonBase} bg-[#ff4500] hover:bg-[#e64500]`}
+            >
+              디에듀 시작하기
+            </Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
