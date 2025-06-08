cd6cb59aaa78cb869b17107758a2dd1b25ac470f
OHS <ohs6006@gmail.com>
2025-06-08 16:24:51 +0900

    chore : 사이드바 경로 수정

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,14 @@
+import { Sidebar } from '@/components/layout/sidebar';
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="flex flex-1 bg-[#F9F9F9]">
+      <Sidebar />
+      <div className="mx-auto max-w-[1200px] flex-1">{children}</div>
+    </div>
+  );
 }
