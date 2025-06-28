e2795cfdd723bcedf9c17257025767a23403f82d
OHS <ohs6006@gmail.com>
2025-06-28 12:36:51 +0900

    feat : 로그페이지 UI

--- src/app/(dashboard)/dashboard/logs/page.tsx
+++ src/app/(dashboard)/dashboard/logs/page.tsx
@@ -1,3 +1,12 @@
+import LogHeader from '@/features/dashboard/log/components/header';
+import RecentlyList from '@/features/dashboard/log/components/recently-list';
+
 export default function LogListPage() {
-  return <main>수업 로그</main>;
+  return (
+    <main className="max-w-[1344px] py-[92px]">
+      <LogHeader />
+      <hr className="mt-[115px] mb-[101px]" />
+      <RecentlyList />
+    </main>
+  );
 }
