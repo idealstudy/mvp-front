f57e840fdc4ce2214a0c5880312075513d80a4a6
김효인 <concert_routers_0l@icloud.com>
2025-07-13 15:57:31 +0900

    feat: add new SVG icons and update header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,37 +3,87 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import profile from '@/../public/header-profile.svg';
-import logo from '@/../public/logo.svg';
 import { ROUTE } from '@/constants/route';
 import { useSession } from '@/features/auth/hooks/use-session';
+import { cn } from '@/lib/utils';
 
 export const Header = () => {
   const { data: session } = useSession();
+
+  const roleMetaMap = {
+    ROLE_STUDENT: {
+      label: '학생',
+      className: 'border-white text-white',
+    },
+    ROLE_PARENT: {
+      label: '부모',
+      className: 'border-[#FFBAA9] text-[#FFBAA9]',
+    },
+    ROLE_TEACHER: {
+      label: '선생님',
+      className: 'border-dedu-orange text-dedu-orange',
+    },
+  } as const;
 
   const buttonBase =
     'cursor-pointer border border-[#1A1A1A] px-6 py-3 text-base font-bold text-white';
 
   return (
     <header className="h-header-height flex items-center border-b border-gray-200 bg-[#1A1A1A] px-4">
-      <div className="mx-auto flex w-full max-w-[1385px] items-center justify-between">
-        <Link href={ROUTE.HOME}>
+      <div className="mx-auto flex w-full items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Link href={ROUTE.HOME}>
+            <Image
+              src={'/logo.svg'}
+              alt="THE EDU 로고"
+              width={79}
+              height={22}
+              className="cursor-pointer"
+            />
+          </Link>
           <Image
-            src={logo}
-            alt="THE EDU 로고"
-            width={79}
-            height={22}
-            className="cursor-pointer"
+            src={'/img_header_beta.svg'}
+            alt="BETA"
+            width={44}
+            height={20}
           />
-        </Link>
+        </div>
         {session && (
-          <Image
-            src={profile}
-            alt="프로필 사진"
-            width={48}
-            height={48}
-            className="rounded-full"
-          />
+          <div className="flex items-center">
+            <Image
+              src={'/img_header_bell.svg'}
+              alt="알림 벨 아이콘"
+              width={24}
+              height={24}
+              className="mr-8 cursor-pointer"
+            />
+            <Image
+              src={'/img_header_profile.svg'}
+              alt="프로필 사진"
+              width={48}
+              height={48}
+              className="mr-4 hidden cursor-pointer rounded-full lg:flex"
+            />
+            <p className="mr-2 hidden items-center gap-2 text-[14px] font-[600] text-white lg:flex">
+              {session.nickname}
+            </p>
+            <div
+              className={cn(
+                'hidden items-center gap-2 rounded-[40px] border px-2 py-[2px] text-[12px] font-[400px] text-[#ffffff] lg:flex',
+                roleMetaMap[session.auth]?.className ??
+                  'border-white text-white'
+              )}
+            >
+              {roleMetaMap[session.auth]?.label ?? ''}
+            </div>
+            <Image
+              src={'/ic_hamburger.svg'}
+              alt="햄버거 메뉴 아이콘"
+              width={24}
+              height={24}
+              className="mr-4 cursor-pointer lg:hidden"
+            />
+          </div>
         )}
         {session === null && (
           <div className="flex gap-5">
