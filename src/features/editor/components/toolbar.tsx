46ff6e1354b07fc0a169200955588db656202a02
nonon <nononcrust@gmail.com>
2025-07-28 15:11:04 +0900

    wip

--- src/features/editor/components/toolbar.tsx
+++ src/features/editor/components/toolbar.tsx
@@ -15,6 +15,11 @@ export const Toolbar = ({ editor }: ToolbarProps) => {
         isUnderline: ctx.editor.isActive('underline'),
         isStrikethrough: ctx.editor.isActive('strike'),
         fontSize: ctx.editor.getAttributes('textStyle').fontSize || '16px',
+        isOrderedList: ctx.editor.isActive('orderedList'),
+        isBulletList: ctx.editor.isActive('bulletList'),
+        isTextAlignLeft: ctx.editor.isActive({ textAlign: 'left' }),
+        isTextAlignCenter: ctx.editor.isActive({ textAlign: 'center' }),
+        isTextAlignRight: ctx.editor.isActive({ textAlign: 'right' }),
       };
     },
   });
@@ -39,27 +44,48 @@ export const Toolbar = ({ editor }: ToolbarProps) => {
     editor.chain().focus().toggleStrike().run();
   };
 
+  const toggleOrderedList = () => {
+    editor.chain().focus().toggleOrderedList().run();
+  };
+
+  const toggleBulletList = () => {
+    editor.chain().focus().toggleBulletList().run();
+  };
+
+  const toggleTextAlignment = (alignment: 'left' | 'center' | 'right') => {
+    editor.chain().focus().toggleTextAlign(alignment).run();
+  };
+
   const fontSizeOptions = [12, 14, 16];
 
   return (
-    <div className="border-line-line2 flex h-[46px] items-center border-b px-3 py-[10px]">
-      <div className="relative">
-        <select
-          className="focus-visible:focus-ring flex h-4 w-[48px] cursor-pointer appearance-none text-xs"
-          value={editorState.fontSize}
-          onChange={(e) => changeFontSize(e.target.value)}
-        >
-          {fontSizeOptions.map((size) => (
-            <option
-              key={size}
-              value={`${size}px`}
-            >
-              {size}pt
-            </option>
-          ))}
-        </select>
-        <ChevronDownIcon className="pointer-events-none absolute top-1/2 right-2 -translate-y-1/2" />
-      </div>
+    <div className="border-line-line2 flex h-[46px] items-center overflow-x-auto border-b">
+      <ToolbarItemGroup>
+        <div className="relative flex items-center gap-1">
+          <select
+            className="focus-visible:focus-ring flex cursor-pointer appearance-none pr-4 text-sm"
+            value={editorState.fontSize}
+            onChange={(e) => changeFontSize(e.target.value)}
+          >
+            {fontSizeOptions.map((size) => (
+              <option
+                key={size}
+                value={`${size}px`}
+              >
+                {size}pt
+              </option>
+            ))}
+          </select>
+          <ChevronDownIcon className="pointer-events-none absolute top-1/2 right-1 size-2 -translate-y-1/2" />
+        </div>
+      </ToolbarItemGroup>
+      <ToolbarDivider />
+      <ToolbarItemGroup>
+        <button className="flex cursor-pointer items-center justify-center gap-[6px] text-xs">
+          글자색
+          <span className="size-4 rounded-full bg-black" />
+        </button>
+      </ToolbarItemGroup>
       <ToolbarDivider />
       <ToolbarItemGroup>
         <ToolbarButton
@@ -87,16 +113,61 @@ export const Toolbar = ({ editor }: ToolbarProps) => {
           <StrikethroughIcon />
         </ToolbarButton>
       </ToolbarItemGroup>
+      <ToolbarDivider />
+      <ToolbarItemGroup>
+        <ToolbarButton
+          active={editorState.isTextAlignLeft}
+          onClick={() => toggleTextAlignment('left')}
+        >
+          <AlignLeftIcon />
+        </ToolbarButton>
+        <ToolbarButton
+          active={editorState.isTextAlignCenter}
+          onClick={() => toggleTextAlignment('center')}
+        >
+          <AlignCenterIcon />
+        </ToolbarButton>
+        <ToolbarButton
+          active={editorState.isTextAlignRight}
+          onClick={() => toggleTextAlignment('right')}
+        >
+          <AlignRightIcon />
+        </ToolbarButton>
+      </ToolbarItemGroup>
+      <ToolbarDivider />
+      <ToolbarItemGroup>
+        <ToolbarButton
+          active={editorState.isBulletList}
+          onClick={toggleBulletList}
+        >
+          <BulletListIcon />
+        </ToolbarButton>
+        <ToolbarButton
+          active={editorState.isOrderedList}
+          onClick={toggleOrderedList}
+        >
+          <ListIcon />
+        </ToolbarButton>
+      </ToolbarItemGroup>
+      <ToolbarDivider />
+      <ToolbarItemGroup>
+        <ToolbarButton>
+          <ImageIcon />
+        </ToolbarButton>
+        <ToolbarButton>
+          <LinkIcon />
+        </ToolbarButton>
+      </ToolbarItemGroup>
     </div>
   );
 };
 
 const ToolbarDivider = () => {
-  return <div className="bg-line-line1 mx-3 h-full w-[1px]" />;
+  return <div className="bg-line-line1 h-full w-[1px]" />;
 };
 
 const ToolbarItemGroup = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex items-center gap-3">{children}</div>;
+  return <div className="flex items-center gap-3 px-3">{children}</div>;
 };
 
 type ToolbarButtonProps = React.ComponentPropsWithRef<'button'> & {
@@ -234,6 +305,150 @@ const StrikethroughIcon = (props: React.SVGProps<SVGSVGElement>) => {
     >
       <path
         d="M3.16699 8.16668H12.8337M12.167 5.50001V5.33334C12.167 4.22877 11.2716 3.33334 10.167 3.33334H5.83366C4.72909 3.33334 3.83366 4.22877 3.83366 5.33334V6.16668C3.83366 7.27125 4.72909 8.16668 5.83366 8.16668H10.0003M3.83366 10.5V10.8333C3.83366 11.9379 4.72909 12.8333 5.83366 12.8333H10.167C11.2716 12.8333 12.167 11.9379 12.167 10.8333V9.83334"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const AlignLeftIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M5.1665 5.83325H11.4998M5.1665 14.1666H11.4998M5.1665 9.99992H14.8332"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const AlignCenterIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M7.1665 5.83325H12.8332M7.1665 14.1666H12.8332M5.1665 9.99992H14.8332"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const AlignRightIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M8.49984 5.83325H14.8332M8.49984 14.1666H14.8332M5.1665 9.99992H14.8332"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const BulletListIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M2.875 7.75C3.91053 7.75 4.75 6.91053 4.75 5.875C4.75 4.83947 3.91053 4 2.875 4C1.83947 4 1 4.83947 1 5.875C1 6.91053 1.83947 7.75 2.875 7.75Z"
+        fill="currentColor"
+      />
+      <path
+        d="M2.875 16.5C3.91053 16.5 4.75 15.6605 4.75 14.625C4.75 13.5895 3.91053 12.75 2.875 12.75C1.83947 12.75 1 13.5895 1 14.625C1 15.6605 1.83947 16.5 2.875 16.5Z"
+        fill="currentColor"
+      />
+      <path
+        d="M8.5 14.625C8.5 14.2798 8.77982 14 9.125 14H16.625C16.9702 14 17.25 14.2798 17.25 14.625C17.25 14.9702 16.9702 15.25 16.625 15.25H9.125C8.77982 15.25 8.5 14.9702 8.5 14.625ZM8.5 5.875C8.5 5.52982 8.77982 5.25 9.125 5.25H16.625C16.9702 5.25 17.25 5.52982 17.25 5.875C17.25 6.22018 16.9702 6.5 16.625 6.5H9.125C8.77982 6.5 8.5 6.22018 8.5 5.875Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+const ListIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M10 14.375C10 14.0298 10.2798 13.75 10.625 13.75H18.125C18.4702 13.75 18.75 14.0298 18.75 14.375C18.75 14.7202 18.4702 15 18.125 15H10.625C10.2798 15 10 14.7202 10 14.375ZM10 5.625C10 5.27982 10.2798 5 10.625 5H18.125C18.4702 5 18.75 5.27982 18.75 5.625C18.75 5.97018 18.4702 6.25 18.125 6.25H10.625C10.2798 6.25 10 5.97018 10 5.625ZM5.625 7.5C5.27982 7.5 5 7.22018 5 6.875V3.125C5 2.77982 4.72018 2.5 4.375 2.5H4.0625C3.88991 2.5 3.75 2.63991 3.75 2.8125C3.75 2.98509 3.61009 3.125 3.4375 3.125H3.125C2.77982 3.125 2.5 3.40482 2.5 3.75C2.5 4.09518 2.77982 4.375 3.125 4.375C3.47018 4.375 3.75 4.65482 3.75 5V6.875C3.75 7.22018 3.47018 7.5 3.125 7.5C2.77982 7.5 2.5 7.77982 2.5 8.125C2.5 8.47018 2.77982 8.75 3.125 8.75H5.625C5.97018 8.75 6.25 8.47018 6.25 8.125C6.25 7.77982 5.97018 7.5 5.625 7.5ZM6.25 16.875C6.25 17.2202 5.97018 17.5 5.625 17.5H4.375C3.33947 17.5 2.5 16.6605 2.5 15.625V15C2.5 14.6685 2.6317 14.3505 2.86612 14.1161C3.10054 13.8817 3.41848 13.75 3.75 13.75H4.375C4.72018 13.75 5 13.4702 5 13.125C5 12.7798 4.72018 12.5 4.375 12.5H3.125C2.77982 12.5 2.5 12.2202 2.5 11.875C2.5 11.5298 2.77982 11.25 3.125 11.25H5C5.33152 11.25 5.64946 11.3817 5.88388 11.6161C6.1183 11.8505 6.25 12.1685 6.25 12.5V13.75C6.25 14.0815 6.1183 14.3995 5.88388 14.6339C5.64946 14.8683 5.33152 15 5 15H4.375C4.02982 15 3.75 15.2798 3.75 15.625C3.75 15.9702 4.02982 16.25 4.375 16.25H5.625C5.97018 16.25 6.25 16.5298 6.25 16.875Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+const ImageIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M5.1665 12.6667L6.9973 10.3379C7.51644 9.67749 8.50952 9.65589 9.05689 10.2931L10.6665 12.1667M9.27653 10.5487C9.96797 9.66919 10.9314 8.42312 10.9941 8.34204L11.0007 8.33352C11.5208 7.6774 12.5107 7.65729 13.0569 8.29306L14.6665 10.1667M6.49984 5.16675H13.4998C14.2362 5.16675 14.8332 5.7637 14.8332 6.50008V13.5001C14.8332 14.2365 14.2362 14.8334 13.4998 14.8334H6.49984C5.76346 14.8334 5.1665 14.2365 5.1665 13.5001V6.50008C5.1665 5.7637 5.76346 5.16675 6.49984 5.16675Z"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const LinkIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M13.167 10.8334L14.0003 10.0001C15.1049 8.89549 15.1049 7.10463 14.0003 6.00006C12.8957 4.89549 11.1049 4.89549 10.0003 6.00006L9.16697 6.83339M6.83364 9.16672L6.0003 10.0001C4.89573 11.1046 4.89573 12.8955 6.0003 14.0001C7.10487 15.1046 8.89573 15.1046 10.0003 14.0001L10.8336 13.1667M11.5003 8.50006L8.5003 11.5001"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
