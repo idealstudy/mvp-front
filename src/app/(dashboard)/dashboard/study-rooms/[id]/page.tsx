af447852d5b9e282f57e8842bc32d7b5c865016c
김효인 <watches876@gmail.com>
2025-08-27 22:09:22 +0900

    by passing handleDialog function to each dialog component.

--- src/app/(dashboard)/dashboard/study-rooms/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/study-rooms/[id]/page.tsx
@@ -1,12 +1,14 @@
 import { ColumnLayout } from '@/components/layout/column-layout';
 import { StudyNotes } from '@/features/studyrooms/studynotes';
+import { StudyRoomTabs } from '@/features/studyrooms/taps';
 
 export default async function StudyRoomDetailPage() {
   return (
     <main>
       <ColumnLayout>
         <ColumnLayout.Left className="h-[400px] rounded-[12px] bg-gray-200" />
-        <ColumnLayout.Right className="desktop:max-w-[740px] h-[400px] rounded-[12px] bg-white px-8 py-6">
+        <ColumnLayout.Right className="desktop:max-w-[740px] flex h-[400px] flex-col gap-3 rounded-[12px] px-8 py-6">
+          <StudyRoomTabs />
           <StudyNotes />
         </ColumnLayout.Right>
       </ColumnLayout>
--- src/features/studyrooms/studynotes/index.tsx
+++ src/features/studyrooms/studynotes/index.tsx
@@ -32,7 +32,7 @@ export const StudyNotes = () => {
   };
 
   return (
-    <div className="flex flex-col gap-6">
+    <div className="border-line-line1 flex flex-col gap-6 rounded-[12px] border bg-white px-8 py-6">
       <div className="flex flex-col gap-3">
         <SearchFilterBar
           search={search}
