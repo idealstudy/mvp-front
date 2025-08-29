18aa3e6e73ba6232ed57c92b6a38269b37b5ef68
김효인 <watches876@gmail.com>
2025-08-29 11:01:45 +0900

    refactor(select): select 기존 컴포넌트로 수정

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -5,6 +5,23 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 import { Select as SelectPrimitives } from 'radix-ui';
 
+type SelectChevronDownIconProps = {
+  className?: string;
+};
+
+export const SelectChevronDownIcon = ({
+  className,
+}: SelectChevronDownIconProps) => {
+  return (
+    <ChevronDownIcon
+      className={cn(
+        'pointer-events-none absolute top-1/2 right-6 size-4 shrink-0 -translate-y-1/2',
+        className
+      )}
+    />
+  );
+};
+
 type SelectProps = React.ComponentPropsWithRef<typeof SelectPrimitives.Root>;
 
 const Select = ({ children, ...props }: SelectProps) => {
@@ -19,21 +36,24 @@ type SelectTriggerProps = React.ComponentPropsWithRef<
 
 const SelectTrigger = ({
   className,
+  'aria-invalid': ariaInvalid,
   placeholder,
   ...props
 }: SelectTriggerProps) => {
   return (
     <SelectPrimitives.Trigger
       className={cn(
-        'border-line-line3 bg-gray-scale-white relative flex h-[36px] items-center justify-between rounded-[8px] border pr-11 pl-3 text-start text-sm outline-hidden',
-        'min-w-[110px]',
-        'data-placeholder:text-text-sub2',
+        'border-gray-scale-gray-10 bg-gray-scale-white relative flex h-[56px] w-full items-center justify-between rounded-[4px] border pr-14 pl-6 text-start outline-hidden',
+        'data-placeholder:text-gray-scale-gray-50',
         '[&>span]:min-w-0',
-        'placeholder-text-text-sub2',
+        'placeholder-text-gray-scale-gray-50',
         'cursor-pointer',
         'data-[state=open]:[&>svg]:rotate-180',
+        //   'disabled:bg-background-100 disabled:pointer-events-none disabled:opacity-50',
+        ariaInvalid && '',
         className
       )}
+      aria-invalid={ariaInvalid}
       {...props}
     >
       <span className="truncate">
@@ -63,7 +83,7 @@ const SelectContent = ({
     <SelectPrimitives.Portal>
       <SelectPrimitives.Content
         className={cn(
-          'border-line-line2 text-main bg-gray-scale-white relative z-50 max-w-[calc(100vw-12px)] overflow-hidden rounded-[6px] border',
+          'border-gray-scale-gray-10 text-main bg-gray-scale-white relative z-50 max-w-[calc(100vw-12px)] overflow-hidden rounded-[4px] border',
           'max-h-[var(--radix-select-content-available-height)]',
           position === 'popper' &&
             'w-full min-w-[var(--radix-select-trigger-width)]',
@@ -76,6 +96,7 @@ const SelectContent = ({
       >
         <SelectPrimitives.Viewport
           className={cn(
+            'divide-gray-scale-gray-10 divide-y',
             position === 'popper' && 'h-[var(--radix-select-trigger-height)]'
           )}
         >
@@ -94,7 +115,7 @@ const SelectOption = ({ className, children, ...props }: SelectOptionProps) => {
   return (
     <SelectPrimitives.Item
       className={cn(
-        'relative flex h-[32px] cursor-pointer items-center justify-center px-3 outline-hidden select-none',
+        'relative flex h-[56px] w-full cursor-pointer items-center px-6 outline-hidden select-none',
         'focus:bg-gray-scale-gray-5',
         'data-[state=checked]:text-key-color-primary',
         'data-disabled:pointer-events-none',
@@ -120,10 +141,13 @@ export const ChevronDownIcon = ({ className }: { className: string }) => {
       height="17"
       viewBox="0 0 16 17"
       fill="none"
-      stroke="currentColor"
     >
+      <title>Chevron down</title>
       <g clipPath="url(#clip0_89_1012)">
-        <path d="M14.772 6.27197L8.27197 12.772L1.77197 6.27197" />
+        <path
+          d="M14.772 6.27197L8.27197 12.772L1.77197 6.27197"
+          stroke="#1A1A1A"
+        />
       </g>
       <defs>
         <clipPath id="clip0_89_1012">
@@ -136,23 +160,6 @@ export const ChevronDownIcon = ({ className }: { className: string }) => {
         </clipPath>
       </defs>
     </svg>
-  );
-};
-
-type SelectChevronDownIconProps = {
-  className?: string;
-};
-
-export const SelectChevronDownIcon = ({
-  className,
-}: SelectChevronDownIconProps) => {
-  return (
-    <ChevronDownIcon
-      className={cn(
-        'text-line-line2 pointer-events-none absolute top-1/2 right-3 size-4 shrink-0 -translate-y-1/2',
-        className
-      )}
-    />
   );
 };
 
