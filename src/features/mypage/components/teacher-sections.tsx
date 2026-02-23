ce2ccc04a1728714f4d191a37859c494230b2400
시원 <sw622@naver.com>
2026-02-23 16:07:07 +0900

    feat: 선생님 활동 요약 api 연결

--- src/features/mypage/components/teacher-sections.tsx
+++ src/features/mypage/components/teacher-sections.tsx
@@ -1,12 +1,28 @@
+import { useTeacherReport } from '@/features/mypage/hooks/teacher/use-report';
 import ComingSoonSection from '@/features/profile/components/coming-soon-section';
 import SectionContainer from '@/features/profile/components/section-container';
+import ActivitySummarySection from '@/features/profile/components/teacher/activity-summary-section';
 import SelectStudynotesDialog from '@/features/profile/components/teacher/select-studynotes-dialog';
+import { MiniSpinner } from '@/shared/components/loading';
 
 export default function TeacherSections() {
+  const {
+    data: report,
+    isLoading: isReportLoading,
+    error: isReportError,
+  } = useTeacherReport();
+
+  // TODO: loading에 skeleton 적용
+  const renderActivitySummary = () => {
+    if (isReportLoading) return <MiniSpinner />;
+    if (isReportError || !report) return <ComingSoonSection />;
+    return <ActivitySummarySection summary={report} />;
+  };
+
   return (
     <>
       <SectionContainer title="활동 요약">
-        <ComingSoonSection />
+        {renderActivitySummary()}
       </SectionContainer>
 
       <SectionContainer title="후기">
--- src/features/profile/components/teacher/activity-summary-section.tsx
+++ src/features/profile/components/teacher/activity-summary-section.tsx
@@ -1,43 +1,59 @@
 const ACTIVITY_SUMMARY_META = [
   {
-    key: 'studyroom',
+    key: 'studyRoomCount',
     label: '운영중인 스터디룸',
     unit: '개',
   },
   {
-    key: 'note',
+    key: 'teachingNoteCount',
     label: '수업노트',
     unit: '개',
   },
   {
-    key: 'student',
+    key: 'studentCount',
     label: '누적 학생 수',
     unit: '명',
   },
   {
-    key: 'review',
+    key: 'reviewCount',
     label: '후기 수',
     unit: '개',
   },
   {
-    key: 'qna',
+    key: 'qnaCount',
     label: '질문 답변 수',
     unit: '개',
   },
-];
+] as const satisfies {
+  key: keyof ActivitySummary;
+  label: string;
+  unit: string;
+}[];
 
-export default function ActivitySummarySection() {
+type ActivitySummary = {
+  studyRoomCount: number;
+  teachingNoteCount: number;
+  studentCount: number;
+  reviewCount: number;
+  qnaCount: number;
+};
+
+export default function ActivitySummarySection({
+  summary,
+}: {
+  summary: ActivitySummary;
+}) {
   const summaryList = ACTIVITY_SUMMARY_META.map((meta) => ({
     ...meta,
-    value: 0,
+    value: summary[meta.key],
   }));
 
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between py-4">
       {summaryList.map((summary) => (
         <div
           key={summary.key}
-          className="p-1"
+          className="space-y-2 p-1"
         >
           <div className="text-key-color-primary flex items-baseline justify-center gap-1">
             <span className="font-title-heading">{summary.value}</span>
