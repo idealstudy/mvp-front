ec1e3ba42ef6b2ffeebe3d189eeb6c433a82a691
LeeJinyoung218 <amy021899@gmail.com>
2026-02-24 22:11:56 +0900

    chore: index.ts로 진입 관리

--- src/features/invite/index.ts
+++ src/features/invite/index.ts
@@ -1,5 +1,2 @@
-export * from '@/features/invite/components/invite-letter';
-export * from '@/features/invite/components/invite-error-content';
-export * from '@/features/invite/components/invite-success-content';
-export * from '@/features/invite/components/invite-login-modal';
+export * from '@/features/invite/components';
 export * from '@/features/invite/types';
