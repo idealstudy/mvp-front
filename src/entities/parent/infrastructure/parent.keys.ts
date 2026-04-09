6b4467afb4ed038925c1d614e78b40bfef532d33
jihwan2001 <coghtoy@naver.com>
2026-04-09 17:26:24 +0900

    feat: parent 엔티티 추가

---
+++ src/entities/parent/infrastructure/parent.keys.ts
@@ -0,0 +1,20 @@
+export const parentKeys = {
+  all: ['parent'] as const,
+  dashboard: {
+    all: () => [...parentKeys.all, 'dashboard'] as const,
+    report: () => [...parentKeys.dashboard.all(), 'report'] as const,
+    connectedStudentList: () =>
+      [...parentKeys.dashboard.all(), 'connectedStudentList'] as const,
+    studyNewsList: (studentId: number) =>
+      [...parentKeys.dashboard.all(), 'studyNewsList', studentId] as const,
+    studyConsultationList: (studyRoomId: number) =>
+      [
+        ...parentKeys.dashboard.all(),
+        'studyConsultationList',
+        studyRoomId,
+      ] as const,
+    studyRoomPreviewList: () =>
+      [...parentKeys.dashboard.all(), 'studyRoomPreviewList'] as const,
+    inquiryList: () => [...parentKeys.dashboard.all(), 'inquiryList'] as const,
+  },
+};
---
+++ src/entities/parent/types/index.ts
@@ -0,0 +1,22 @@
+import { dto } from '@/entities/parent/infrastructure/parent.dto';
+import { z } from 'zod';
+
+/*
+ * DTO
+ */
+export type ParentDashboardConnectedStudentListDTO = z.infer<
+  typeof dto.dashboard.connectedStudentList
+>;
+export type ParentDashboardReportDTO = z.infer<typeof dto.dashboard.report>;
+export type ParentDashboardStudyNewsListDTO = z.infer<
+  typeof dto.dashboard.studyNewsList
+>;
+export type ParentDashboardConsultationListDTO = z.infer<
+  typeof dto.dashboard.consultationList
+>;
+export type ParentDashboardStudyRoomPreviewListDTO = z.infer<
+  typeof dto.dashboard.studyRoomPreviewList
+>;
+export type ParentDashboardInquiryListDTO = z.infer<
+  typeof dto.dashboard.inquiryList
+>;
